27b2991b39de78180a5cd846d1b2a7b1c6d13939
moustapha <kabermoustapha@gmail.com>
2018-07-10 22:03:25 +0200

    merge with master
    # Conflicts:
    #	src/main/java/mr/mbconsulting/Sygep/model/Client.java

--- src/main/java/mr/mbconsulting/Sygep/model/Client.java
+++ src/main/java/mr/mbconsulting/Sygep/model/Client.java
@@ -7,49 +7,57 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Client implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-
 	private Long id;
-	public Client(String nom, List<Contrat> contrats) {
-		super();
-		this.nom = nom;
-		this.contrats = contrats;
-	}
-	public Client(String nom) {
-		super();
-		this.nom = nom;
-	}
+
+    private String nom;
+    @OneToMany(mappedBy="client")
+    private List<Contrat>  contrats=new ArrayList<>();
+
+    private String prenom;
+
+
 	public Client() {
 		super();
-		// TODO Auto-generated constructor stub
-	}
-	public List<Contrat> getContrats() {
-		return contrats;
-	}
-	public void setContrats(List<Contrat> contrats) {
-		this.contrats = contrats;
 	}
 
-	private String nom;
-	@OneToMany(mappedBy="client")
-	private List<Contrat>  contrats=new ArrayList<>();
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public void addContrat(Contrat contrat){
-		this.getContrats().add(contrat);
-		contrat.setClient(this) ;
-	}
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public List<Contrat> getContrats() {
+        return contrats;
+    }
+
+    public void setContrats(List<Contrat> contrats) {
+        this.contrats = contrats;
+    }
 }
